f2b464a17c176ad37b513bcf57bf490d8f24c6da
Andiara Rios <andiara.rios@gmail.com>
2025-06-26 12:52:15 -0300

    atualização de banner componente Banner.jsx e style-banner.css

--- explorador/src/components/Banner.jsx
+++ explorador/src/components/Banner.jsx
@@ -1,19 +1,24 @@
+import bannerImage from '../assets/banner.jpg';
+
 function Banner() {
   return (
-    <div className="card banner text-bg-dark">
-      <img src="/img/banner.jpg" className="card-img" alt="Banner principal" />
+    <div className="card banner text-white">
+      <img src={bannerImage} className="card-img" alt="Banner principal" />
       <div className="card-img-overlay d-flex flex-column justify-content-center text-center">
-        <h1 className="card-title bg-black bg-opacity-50 p-2 rounded">
-          Descubra o Mundo com a Gente
-        </h1>
-        <p className="card-text bg-dark bg-opacity-50 p-2 rounded">
-          Viajar é mais do que ir de um lugar a outro – é sobre viver
-          experiências que transformam. Aqui, criamos roteiros e dicas para quem
-          deseja explorar o mundo.
-        </p>
-        <p className="card-text bg-dark bg-opacity-50 p-1 rounded">
-          <small>Pronto para começar sua próxima aventura? Explore nossos destinos!</small>
-        </p>
+        <h1 className="card-title banner-texto banner-texto-1" style={{ color: '#ff6600', fontSize: '2.8rem' }}>
+  Descubra o Mundo com a Gente
+</h1>
+
+<p className="card-text banner-texto banner-texto-2" style={{ fontSize: '1.2rem', color: '#ffffff' }}>
+  Viajar é mais do que ir de um lugar a outro – é sobre viver
+  experiências que transformam. Aqui, criamos roteiros e dicas para quem
+  deseja explorar o mundo.
+</p>
+
+<p className="card-text banner-texto banner-texto-3" style={{ fontSize: '1rem', color: '#eeeeee' }}>
+  <small>Pronto para começar sua próxima aventura? Explore nossos destinos!</small>
+</p>
+
       </div>
     </div>
   );
